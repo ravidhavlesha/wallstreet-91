1050291712c76f7064c4a142bcd4f49e7a99f38d
Ravi Dhavlesha <ravidhavlesha@gmail.com>
2020-07-26 18:26:42 +0530

    chore: deployment

--- server/index.js
+++ server/index.js
@@ -18,11 +18,11 @@ app.use(helmet());
 const UPDATE_INTERVAL = process.env.UPDATE || 5000;
 updateStocks(UPDATE_INTERVAL);
 
-// app.use(express.static('public'));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public/index.html'));
-// });
-app.use(express.static('./client/build'));
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+// app.use(express.static('client/build'));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
 // });
