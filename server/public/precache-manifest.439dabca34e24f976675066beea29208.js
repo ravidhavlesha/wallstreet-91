611453b8721d0a9598d0d2ba65ccee8d38ea3962
Ravi Dhavlesha <ravidhavlesha@gmail.com>
2020-07-26 18:55:00 +0530

    chore: deployment

---
+++ server/public/precache-manifest.439dabca34e24f976675066beea29208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50290b3b723b67089eabfbb7448afdba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d0b8bfe9b8debc5c165",
+    "url": "/static/js/2.41982b08.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.41982b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958733569909800ed130",
+    "url": "/static/js/main.555e431d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
