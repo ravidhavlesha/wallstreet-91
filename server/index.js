76a2629b761429cb4e207c76551b6280856c8c56
Ravi Dhavlesha <ravidhavlesha@gmail.com>
2020-07-26 18:42:39 +0530

    chore: deployment

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(helmet());
 // app.get('*', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'public/index.html'));
 // });
-app.use(express.static('client/build'));
+app.use(express.static(path.resolve('client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('client/build/index.html'));
 });
