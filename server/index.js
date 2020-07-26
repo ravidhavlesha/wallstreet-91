adc4fe2ee44e79d4e55a32423d4b283163262629
Ravi Dhavlesha <ravidhavlesha@gmail.com>
2020-07-26 18:04:21 +0530

    chore: deployment

--- server/index.js
+++ server/index.js
@@ -18,10 +18,14 @@ app.use(helmet());
 const UPDATE_INTERVAL = process.env.UPDATE || 5000;
 updateStocks(UPDATE_INTERVAL);
 
-app.use(express.static('/var/task/client/build'));
+app.use(express.static('public'));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/var/task/client/build/index.html'));
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
+// app.use(express.static('client/build'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+// });
 
 const APP_NAME = process.env.APP_NAME || 'Wallstreet91';
 
--- server/utils/db-connection.js
+++ server/utils/db-connection.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose, (Promise = global.Promise);
+mongoose.Promise = global.Promise;
 const dbConfig = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true };
-const dbConnection = mongoose.connect(process.env.MONGO_URI || '', dbConfig);
+const dbConnection = mongoose
+  .connect(process.env.MONGO_URI || '', dbConfig)
+  .then((conn) => {
+    console.log('Successfully connected to mongo');
+    return conn;
+  })
+  .catch((err) => {
+    console.error(`Error while attempting to connect to mongo with message: ${err.message}`);
+  });
 
 module.exports = dbConnection;
